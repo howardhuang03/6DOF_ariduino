8523109b5b1d5904c58e072e575060bb013ddf99
Howardhuang <howard_huang@htc.com>
2016-09-10 17:18:13 +0800

    Add sampling rate calculation
    Signed-off-by: Howardhuang <howard_huang@htc.com>

--- fitknee_arduino.ino
+++ fitknee_arduino.ino
@@ -1,6 +1,11 @@
 #include <Wire.h>
 #include <GY80.h>
+#include <ArduinoJson.h>
 #include <SoftwareSerial.h>
+
+#define TX_PIN 8
+#define RX_PIN 9
+#define threshold 5
 
 struct FitkneeInfo {
     GY80_single_scaled acc;
@@ -8,34 +13,45 @@ struct FitkneeInfo {
 };
 
 GY80 sensor = GY80();     // Create GY80 instance
-SoftwareSerial BT(8, 9);  // Setup BT transmit & receive pin
+SoftwareSerial BT(RX_PIN, TX_PIN);  // Setup BT transmit & receive pin
+StaticJsonBuffer<200> jsonBuffer;
+unsigned long time;
+unsigned long count;
 
 void setup()
 {
-    // initialize serial communication at 115200 bits per second:
+    // Initialize serial communication at 115200 bits per second:
     Serial.begin(9600);
 
+    // Initialize sensors
     Serial.println("Initial 9 DOF sensor");
-    sensor.begin();       //initialize sensors
+    sensor.begin();
 
+    // Initialize another serial port for BT communication
+    // 38400 for HC-05
     Serial.println("Initial 9 BT serial");
-    BT.begin(38400);      // 38400 for HC-05
+    BT.begin(38400);
     
-    Serial.println("Bluetooh &  9 DOF sensor ready!!");
+    Serial.println("External module ready!!");
+
+    // Initialize global variable
+    time = millis();
+    count = 0;
 }
 
-void SensorPrint(GY80_single_scaled acc, GY80_single_scaled gyro) {
+void SensorPrint(unsigned long ts, GY80_single_scaled acc, GY80_single_scaled gyro) {
     // Accelerometer values
-    Serial.print("Acc:");
+    Serial.print("Timestamp:");
+    Serial.print(ts);
+    Serial.print(" Acc:");
     Serial.print(acc.x,3);
     Serial.print(',');
     Serial.print(acc.y,3);
     Serial.print(',');
     Serial.print(acc.z,3);
-    Serial.print(' ');
 
     // Gyroscope values
-    Serial.print("Gyro:");
+    Serial.print(" Gyro:");
     Serial.print(gyro.x,1);
     Serial.print(',');
     Serial.print(gyro.y,1);
@@ -43,16 +59,36 @@ void SensorPrint(GY80_single_scaled acc, GY80_single_scaled gyro) {
     Serial.println(gyro.z,1);
 }
 
+void SamplingRate(float diff) {
+    // Count sample rate
+    float rate = count / diff;
+    Serial.print("Sampling count: ");
+    Serial.print(count);
+    Serial.print(", Sampling rate: ");
+    Serial.println(rate, 6);
+    time = millis();
+    count = 0;
+}
+
 void loop()
 {
     char val_input;
+    unsigned long current = millis();
     FitkneeInfo info;
+
     // Get values from acc & gyro sensors
     info.acc = sensor.a_read_scaled();
     info.gyro = sensor.g_read_scaled();
+    count++;
 
     // Print out acc & gyro values
-    SensorPrint(info.acc, info.gyro);
+    //SensorPrint(current, info.acc, info.gyro);
+
+    // Calculate sampling rate
+    float diff = (current - time) / 1000; 
+    if (diff > threshold) { 
+        SamplingRate(diff);
+    }
 
     // Print out BT data to serial
     if (BT.available()) {
